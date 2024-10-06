0e7d8d58bf379e2b7e37f39aa04ce0a748b44b13
Mendrika Rs <120551193+projetETU@users.noreply.github.com>
2024-10-06 20:26:39 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
+import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 api_token = os.getenv('API_TOKEN')
-print(api_token)
+
+url = f"https://data.fixer.io/api/latest?access_key={api_token}"
+querry = {"base":"EUR","symbols":"MGA"}
+
+response = requests.get(url,params=querry)
+
+print(response.json())
